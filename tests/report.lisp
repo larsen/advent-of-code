8fc626d99a9ad63de20988261e2668f969974d4d
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-10-15 18:21:38 +0200

    Better tests reporting routine

--- tests/report.lisp
+++ tests/report.lisp
@@ -14,7 +14,6 @@
                     (rest l)))
           lst))
 
-
 (defun all-results (test)
   (loop for r across (results test)
         collect r))
@@ -26,12 +25,22 @@
   (null (parent (expression test))))
 
 (defun aoc-daily-tests-recap (tests)
-  (loop for test in tests
+  (loop for test across tests
         collect (coerce (loop for inner-test across test
                               collect (cond
+                                        ((and (eql :passed (status inner-test))
+                                              (eql 'controlling-result (type-of inner-test))
+                                              (eql :skipped (child-status inner-test))) #\s)
                                         ((eql :passed (status inner-test)) #\*)
                                         ((eql :failed (status inner-test)) #\f)))
                         'string)))
+
+(defun test-name->day-number (test-name)
+  "Takes a name like AOC2018/DAY3 (passed as a symbol) and returns the
+corresponding day number (3)"
+  (register-groups-bind ((#'parse-integer day))
+      ("\(\\d+\)\$" (symbol-name test-name))
+    day))
 
 (defmethod summarize ((report aoc-report))
   ;; For AOC tests, we have a three-level hierarchy:
@@ -46,17 +55,28 @@
     (dolist (parent-test (remove-if-not #'has-parent
                                         (filter-results (all-results report)
                                                         'test-result)))
-      (setf (gethash (name (expression parent-test)) test-hierarchy) '()))
+      (setf (gethash (name (expression parent-test)) test-hierarchy)
+            (make-array 25 :initial-element #())))
     ;; ... then their values
     (dolist (test (remove-if #'has-parent
                              (filter-results (all-results report)
                                              'test-result)))
-      (push (results test) (gethash (name (parent (expression test))) test-hierarchy)))
+      (setf (aref (gethash (name (parent (expression test))) test-hierarchy)
+                  (- (test-name->day-number (name (expression test))) 1))
+            (results test)))
     ;; Now pretty-printing the structure
+    (format (output report)
+            "~&~%
+*: Problem solved
+s: Test skipped
+f: Failed test
+
+")
     (print-table (loop for k being the hash-keys of test-hierarchy
                        for v being the hash-values of test-hierarchy
                        collect (append (list k)
-                                       (aoc-daily-tests-recap v)))))
+                                       (aoc-daily-tests-recap v)))
+                 (output report)))
   report)
 
 (defun run-aoc-tests ()
